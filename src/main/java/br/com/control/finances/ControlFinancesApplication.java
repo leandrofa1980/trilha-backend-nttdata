0b65686d88dea72b89d5482d410a9d0fea4ea5ba
Leandro Ferreira <leandrofa1980@gmail.com>
2022-01-10 14:29:46 -0300

    First Commit

---
+++ src/main/java/br/com/control/finances/ControlFinancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.control.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/control/finances/ControlFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.control.finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
